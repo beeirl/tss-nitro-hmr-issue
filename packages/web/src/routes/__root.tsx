aeb95cc5b6be19a468a2cac2356b9014f979ae05
BEEIRL <c@beeirl.com>
2025-12-11 23:02:12 +0100

    sync

--- packages/web/src/routes/__root.tsx
+++ packages/web/src/routes/__root.tsx
@@ -20,7 +20,7 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "TanStack Bug",
+        title: "TSS Nitro HMR Issue",
       },
     ],
     links: [{ rel: "stylesheet", href: styles }],
